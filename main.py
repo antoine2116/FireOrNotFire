b0d7c7964d962344a62c542e86ea574f4ed1f4be
Ilias <ilias.kelesoglu@globule.net>
2022-04-15 12:47:58 +0200

    Commenting validation

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torch.optim import Adam
-import time
+import matplotlib.pyplot as pp
 
 from model import VGG16
 
 lossArray = []
 accArray = []
+lossValArray = []
+accValArray = []
+
+batch_size = 4
+epochs = 2
 PATH = "fire_or_not_fire.pth"
 
 def main():
-    batch_size = 4
-    epochs = 10
 
     classes = ["Fire", "NoFire"]
 
     transform = transforms.Compose([
         transforms.Resize([256, 256]),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.4, 0.4, 0.4], std=[0.2, 0.2, 0.2])
     ])
@@ -31,7 +33,7 @@ def main():
 
     testdata = ImageFolder('Test_Dataset', transform=transform)
     testloader = DataLoader(testdata, batch_size=batch_size, shuffle=True, pin_memory=True, drop_last=False)
-
+    testset_size = len(testdata)
     criterion = nn.CrossEntropyLoss()
 
     model = VGG16()
@@ -84,38 +86,54 @@ def main():
             best_accuracy = epoch_acc
             saveModel(model)
 
-    with torch.no_grad():
-        n_correct = 0
-        n_samples = 0
-        n_class_correct = [0 for i in range(10)]
-        n_class_samples = [0 for i in range(10)]
-        for images, labels in testloader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            # max returns (value ,index)
-            _, predicted = torch.max(outputs, 1)
-            n_samples += labels.size(0)
-            n_correct += (predicted == labels).sum().item()
+    # with torch.no_grad():
+    #     n_correct = 0
+    #     n_samples = 0
+    #     n_class_correct = [0 for i in range(10)]
+    #     n_class_samples = [0 for i in range(10)]
+    #     for images, labels in testloader:
+    #         images = images.to(device)
+    #         labels = labels.to(device)
+    #         outputs = model(images)
+    #         # max returns (value ,index)
+    #         _, predicted = torch.max(outputs, 1)
+    #         n_samples += labels.size(0)
+    #         n_correct += (predicted == labels).sum().item()
+    #
+    #         for i in range(2):
+    #             label = labels[i]
+    #             pred = predicted[i]
+    #             if label == pred:
+    #                 n_class_correct[label] += 1
+    #             n_class_samples[label] += 1
+    #
+    #     acc = 100.0 * n_correct / n_samples
+    #     print(f'Accuracy of the network: {acc} %')
+    #
+    #     for i in range(10):
+    #         acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+    #         print(f'Accuracy of {classes[i]}: {acc} %')
 
-            for i in range(batch_size):
-                label = labels[i]
-                pred = predicted[i]
-                if label == pred:
-                    n_class_correct[label] += 1
-                n_class_samples[label] += 1
-
-        acc = 100.0 * n_correct / n_samples
-        print(f'Accuracy of the network: {acc} %')
-
-        for i in range(10):
-            acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-            print(f'Accuracy of {classes[i]}: {acc} %')
-
-
+        setGraph()
 
 def saveModel(model):
     torch.save(model.state_dict(), PATH)
+
+def setGraph():
+    #Loss
+    loss = pp
+    acc = pp
+    loss.plot(epochs, lossArray)
+    loss.ylabel('Loss %')
+    loss.xlabel('Epoch')
+    loss.show()
+
+    acc.plot(epochs, accArray)
+    acc.ylabel('Accuraccy %')
+    acc.xlabel('Epoch')
+    acc.show()
+
+
 
 if __name__ == '__main__':
     main()
