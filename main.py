f55a9468d7e7a8b29e1b8cfaaf79cd884ba3c005
Ilias <ilias.kelesoglu@globule.net>
2022-04-15 11:30:30 +0200

    BIMBUM

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import torchvision.transforms as transforms
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
+from torch.optim import Adam
+import time
 
 from model import VGG16
 
+lossArray = []
+accArray = []
+PATH = "fire_or_not_fire.pth"
 
 def main():
     batch_size = 4
@@ -15,6 +20,7 @@ def main():
 
     transform = transforms.Compose([
         transforms.Resize([256, 256]),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.4, 0.4, 0.4], std=[0.2, 0.2, 0.2])
     ])
@@ -29,10 +35,14 @@ def main():
     criterion = nn.CrossEntropyLoss()
 
     model = VGG16()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    model.load_state_dict(torch.load(PATH))
+    optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
 
-    device = torch.device("cpu")
+    device = torch.device("cuda")
+    print("The model will be running on", device, "device\n")
+    model.to(device)
 
+    best_accuracy = 0.0
     for epoch in range(epochs):
         print('Epoch {}/{}'.format(epoch, epochs - 1))
         print('-' * 10)
@@ -40,6 +50,8 @@ def main():
         model.train()  # Set model to training mode
         running_loss = 0.0
         running_corrects = 0
+        epoch_loss = 0
+        epoch_acc = 0
 
         # Iterate over data.
         for inputs, labels in trainloader:
@@ -60,12 +72,17 @@ def main():
             running_loss += loss.item() * inputs.size(0)
             running_corrects += torch.sum(preds == labels.data)
 
-            epoch_loss = running_loss / dataset_size
-            epoch_acc = running_corrects.double() / dataset_size
+            epoch_loss = (running_loss / dataset_size) * 100
+            epoch_acc = (running_corrects.double() / dataset_size) * 100
             print('{} Loss: {:.4f} Acc: {:.4f}'.format('train', epoch_loss, epoch_acc))
-        print()
 
-    torch.save(model.state_dict(), 'fire_or_not_fire.pth')
+        print('Epoch stats : Loss = {}, Accuracy = {}'.format(epoch_loss, epoch_acc))
+        lossArray.append(epoch_loss)
+        accArray.append(epoch_acc)
+
+        if epoch_acc > best_accuracy :
+            best_accuracy = epoch_acc
+            saveModel(model)
 
     with torch.no_grad():
         n_correct = 0
@@ -95,6 +112,10 @@ def main():
             acc = 100.0 * n_class_correct[i] / n_class_samples[i]
             print(f'Accuracy of {classes[i]}: {acc} %')
 
+
+
+def saveModel(model):
+    torch.save(model.state_dict(), PATH)
 
 if __name__ == '__main__':
     main()
